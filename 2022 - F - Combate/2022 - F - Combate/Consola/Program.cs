04c9228fa40958bab4687c85e115f1f0aaae9a55
Vizgarra Livon <daniel.vizgarralivon@kcc.com>
2022-08-03 17:42:32 -0300

    iniciando modelo Combate

---
+++ 2022 - F - Combate/2022 - F - Combate/Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine(rnd.TirarMoneda());
+            Console.WriteLine(rnd.TirarMoneda());
+            Console.WriteLine(rnd.TirarMoneda());
+            Console.WriteLine(rnd.TirarMoneda());
+
+
+            Console.ReadLine();
+        }
+    }
+}

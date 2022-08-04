95c17d1633d16042afa97daf2e133248ef2a8f44
Vizgarra Livon <daniel.vizgarralivon@kcc.com>
2022-08-04 10:15:42 -0300

    agrego practica de eventos

---
+++ eventos/ClassLibrary1/ClassLibrary1/ClassPrueba.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public delegate void miDelegado1(int a, int b);
+    public delegate void miDelegado2();
+    public delegate void miDelegado3(string m);
+
+    public static class ClassPrueba
+    {
+        public static event miDelegado1 eventoUno;
+        public static event miDelegado2 eventoDos;
+        public static event miDelegado3 eventoTres;
+
+        public static int valor;
+        public static string nombre;
+        public static string msg;
+        public static string rta;
+
+        static void MetodoY() { msg = "Me cago en estos eventos del orto!!!"; }
+        static void MetodoX() { nombre = "Coco Cocardi"; }
+        static void MetodoZ(int num1, int num2) { valor = num1 + num2; }
+        static void MetodoW(string s) { rta = s+"\n-Si, lo juro!"; }
+
+        public static void InvocarEventos()
+        {
+            eventoUno.Invoke(1, 3);
+            eventoDos?.Invoke();
+        }
+
+        static ClassPrueba()
+        {
+            eventoUno += MetodoZ;
+            eventoDos += MetodoX;
+            eventoDos += MetodoY;
+            eventoTres += MetodoW;
+        }
+        public static void InvocarEventosMsg()
+        {
+            eventoDos?.Invoke();
+        }
+
+        public static void InvocarEventosRta(string s)
+        {
+            eventoTres?.Invoke(s);
+        }
+
+    }
+}
---
+++ eventos/ClassLibrary1/miForm/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace miForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("HOLa Mundo!");
+            ClassPrueba.InvocarEventosMsg();
+            MessageBox.Show(ClassPrueba.msg);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClassPrueba.InvocarEventos();
+            MessageBox.Show("nombre: "+ClassPrueba.nombre +"\nvalor: "+ClassPrueba.valor);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string m = this.richTextBox1.Text;
+            ClassPrueba.InvocarEventosRta(m);
+            this.richTextBox1.Text = ClassPrueba.rta;
+        }
+    }
+}
